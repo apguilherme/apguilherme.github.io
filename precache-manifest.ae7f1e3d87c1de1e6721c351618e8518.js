be3b1f7f6bbf3d43f43f581ed4af0d34aa929a17
Guilherme <guilherme_070@hotmail.com>
2020-01-24 05:14:22 +0000

    Updates

---
+++ precache-manifest.ae7f1e3d87c1de1e6721c351618e8518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e1e667ab4ceb92fb398d946afff21b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "932c1b7fb8f6d2b47eba",
+    "url": "/static/css/main.b97ae6e9.chunk.css"
+  },
+  {
+    "revision": "deeaa6e2e0c504eb3306",
+    "url": "/static/js/2.be0b7ecd.chunk.js"
+  },
+  {
+    "revision": "932c1b7fb8f6d2b47eba",
+    "url": "/static/js/main.a1a7d8f3.chunk.js"
+  },
+  {
+    "revision": "eaa23c1812d062979174",
+    "url": "/static/js/runtime~main.4f1073dc.js"
+  }
+]);
